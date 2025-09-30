c8cf2232516ef075792a0a87f085ab5f59bdab75
attilamester <attilamester21@gmail.com>
2025-09-30 12:26:19 +0300

    Zotero fetch script

---
+++ scripts/zotero.py
@@ -0,0 +1,45 @@
+import os
+
+import requests
+import yaml
+
+
+def get_project_root():
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+def fetch_zotero(yml_path):
+    bibs = []
+    start = 0
+    limit = 100
+
+    while True:
+        url = f"https://api.zotero.org/groups/2770680/collections/YREG49H9/items?format=json&sort=date&start={start}&limit={limit}"
+        try:
+            response = requests.get(url)
+            bib = response.json()
+            if not bib:
+                break
+        except requests.RequestException as e:
+            break
+
+        bibs.extend(bib)
+        start += limit
+    print(f"Fetched {len(bibs)} items from Zotero.")
+    print(f"Writing to {yml_path}")
+
+    # save only title, url, date, authors
+    bibs = [{
+        "DOI": bib["data"].get("DOI", ""),
+        "title": bib["data"].get("title", ""),
+        "url": bib["data"].get("url", ""),
+        "date": bib["data"].get("date", ""),
+        "creators": bib["data"].get("creators", []),
+    } for bib in bibs]
+
+    with open(yml_path, "w") as f:
+        yaml.dump(bibs, f, allow_unicode=True)
+
+
+if __name__ == "__main__":
+    fetch_zotero(os.path.join(get_project_root(), "_data/zotero.yml"))
